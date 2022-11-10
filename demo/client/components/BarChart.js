ff6cd8bb295d055a49dd85f098136dbeb0ac10bb
andyz5313 <Your Emailand>
2022-11-10 08:58:30 -0500

    create query time array and bar component

---
+++ demo/client/components/BarChart.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Bar } from 'react-chartjs-2';
+
+const BarChart = () => {
+
+    return (
+        <div>
+          
+        </div>
+    )
+}
+
+export default BarChart;
--- demo/client/components/Demo.js
+++ demo/client/components/Demo.js
@@ -2,52 +2,86 @@ import React, { useState, useEffect } from 'react';
 
 const Demo = () => {
     const [queryData, setQueryData] = useState([]);
+    const [queryResult, setQueryResult] = useState('');
     const [queryString, setQueryString] = useState(`{ cities  { id  name population country_id } }`);
     const [queryTime, setQueryTime] = useState(0);
+    const [queryTimeArray, setQueryTimeArray] = useState([]);
+    const [chartData, setChartData] = useState({
+        labels: [1, 2, 3],
+        datasets : [{
+            label: 'Query Time in Milliseconds',
+            data: queryTimeArray.map((data) => data)
+        }]
+
+    });
 
     useEffect(() => {
-        console.log("start")
-        async function fetchData () {
-            const startTime = performance.now();
-            console.log(queryString);
-            await fetch('http://localhost:3000/graphql', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                Accept: 'application/json',
-            },
-            body: JSON.stringify({
-                
-                query: queryString,
-            }),
-        })
-        .then((res) => {
-            return res.json();
-        })
-        .then((data) => {
-            const endTime = performance.now() - startTime;
-            setQueryTime(endTime);
-            setQueryData(data.data.cities)
-            //setQueryData(JSON.stringify(data, null, 2));
-            
-        })
-        }
-        
-        fetchData();
+        // console.log("start")
+        // fetchData();
     }, []);
+
+    const fetchData = async () => {
+        const startTime = performance.now();
+        console.log(queryString);
+        await fetch('http://localhost:3000/graphql', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+        },
+        body: JSON.stringify({
+            query: queryString,
+        }),
+    })
+    .then((res) => {
+        return res.json();
+    })
+    .then((data) => {
+        const endTime = performance.now() - startTime;
+        setQueryTime(endTime);
+        setQueryTimeArray([...queryTimeArray, endTime]);
+        setQueryData(data.data.cities)
+        //console.log(JSON.stringify(data, null, 2));
+        setQueryResult(JSON.stringify(data, null, 2))
+        
+    })
+    }
 
     const displayData = () => {
         return queryData.map((item, i) => {
             return <div key={i}>{JSON.stringify(item)}</div>
         })
     }
+    const displayQueryTimeArray = () => {
+        return queryTimeArray.map((item, i) => {
+            return <div key={i}>{item}</div>
+        })
+    }
+
+    const handleQuery = () => {
+        fetchData();
+    }
 
 
 
     return (
         <div>
+          <button id='queryButton' onClick={handleQuery}>Run Query</button>
+          <div>
+            Query Time Array
+            {displayQueryTimeArray()}
+          </div>
           <h1>Fetch Time: {queryTime}</h1>
-          <div className="displayData">
+          <div id='queryString'> 
+            <h2>Query String</h2>
+            {queryString}
+          </div>
+          <div className='queryResult'>
+            <h2>Query Result</h2>
+            {queryResult}
+          </div>
+          <div className='displayData'>
+            <h2>Display Data</h2>
             {displayData()}
           </div>
             
