90fb308032f2d13e157ffb73edd36cd8b715b29c
Dhruv Thota <dthota8@gmail.com>
2022-11-15 17:56:34 -0600

    Landing page routes working + particle physics

--- demo/client/App.js
+++ demo/client/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Demo from './components/Demo';
 import Landing from './components/Landing.js';
 
 const App = () => {
   return (
-  
+    // <BrowserRouter>
       <Routes>
         <Route path='/' element={<Demo />}></Route>
         <Route path='/landing' element={<Landing />}></Route>
       </Routes>
-
+    // </BrowserRouter>
   );
 };
 
--- demo/client/index.js
+++ demo/client/index.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.js';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, NavLink } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.querySelector('#root'));
 root.render(
-    <React.StrictMode>
-      <BrowserRouter>
-        <App />
-     </BrowserRouter>
-    </React.StrictMode>
+
+<div className='NavBar'>
+  <React.StrictMode>
+    <BrowserRouter>
+
+  <nav className='nav'>
+    <ul>
+      <li><NavLink to={'/'}>Demo</NavLink></li>
+      <li><NavLink to={'/landing'}>Landing</NavLink></li>
+    </ul>
+  </nav>
+
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+
+</div>
     
 );
