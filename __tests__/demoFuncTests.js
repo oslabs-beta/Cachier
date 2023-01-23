6aec7f3048fa6efa0534364a6b86d4fb79c2c930
Dhruv Thota <dhruv@dhruvs-mbp.lan>
2023-01-23 00:06:58 -0600

    tests for demoFunc moved to tests folder

--- __tests__/demoFuncTests.js
+++ __tests__/demoFuncTests.js
@@ -1,59 +1,59 @@
 this.capacity = capacity;
-  this.groupSize = groupSize;
-  this.redisClient = redisClient;
-  this.endpoint = endpoint;
+this.groupSize = groupSize;
+this.redisClient = redisClient;
+this.endpoint = endpoint;
 
-  // tests for CacheMoney function
-  test("CacheMoney function should throw an error if capacity or groupSize is less than 1 or groupSize exceeds capacity", () => {
-    expect(() => new CacheMoney(endpoint, 0, 1, redisClient)).toThrowError(
-      "Capacity and groupSize needs to be a number greater than 1 and groupsize cannot exceed capacity"
-    );
+// tests for CacheMoney function
+test("CacheMoney function should throw an error if capacity or groupSize is less than 1 or groupSize exceeds capacity", () => {
+  expect(() => new CacheMoney(endpoint, 0, 1, redisClient)).toThrowError(
+    "Capacity and groupSize needs to be a number greater than 1 and groupsize cannot exceed capacity"
+  );
+});
+
+test("CacheMoney should create a new EvictionQueue", () => {
+  const cacheMoney = new CacheMoney(endpoint, capacity, groupSize, redisClient);
+  expect(cacheMoney.queue).toBeInstanceOf(EvictionQueue);
+});
+
+test("CacheMoney should assign capacity, groupSize, redisClient and endpoint correctly", () => {
+  const cacheMoney = new CacheMoney(endpoint, capacity, groupSize, redisClient);
+  expect(cacheMoney.capacity).toBe(capacity);
+  expect(cacheMoney.groupSize).toBe(groupSize);
+  expect(cacheMoney.redisClient).toBe(redisClient);
+  expect(cacheMoney.endpoint).toBe(endpoint);
+});
+
+test("CacheMoney should have traverse function", () => {
+  const cacheMoney = new CacheMoney(endpoint, capacity, groupSize, redisClient);
+  expect(typeof cacheMoney.traverse).toBe('function');
+});
+
+describe('checkCache', () => {
+  beforeEach(() => {
+    const req = {
+      body: {
+        query: 'some query',
+        variables: { some: 'variables' },
+      }
+    };
+    res = jest.fn();
+    next = jest.fn();
   });
 
-  test("CacheMoney should create a new EvictionQueue", () => {
-    const cacheMoney = new CacheMoney(endpoint, capacity, groupSize, redisClient);
-    expect(cacheMoney.queue).toBeInstanceOf(EvictionQueue);
+  test('should set valueFromCache if redisClient is used', async () => {
+    const redisClient = {
+      get: jest.fn().mockReturnValue('someValue')
+    }
+    await checkCache(req, res, next);
+    expect(redisClient.get).toHaveBeenCalled();
+    expect(valueFromCache).toEqual('someValue');
   });
 
-  test("CacheMoney should assign capacity, groupSize, redisClient and endpoint correctly", () => {
-    const cacheMoney = new CacheMoney(endpoint, capacity, groupSize, redisClient);
-    expect(cacheMoney.capacity).toBe(capacity);
-    expect(cacheMoney.groupSize).toBe(groupSize);
-    expect(cacheMoney.redisClient).toBe(redisClient);
-    expect(cacheMoney.endpoint).toBe(endpoint);
+  test('should set valueFromCache if cacheMoneyCache is used', async () => {
+    await checkCache(req, res, next);
+    expect(cacheMoneyCache[cacheKey]).toBeDefined();
+    expect(valueFromCache).toEqual(cacheMoneyCache[cacheKey]);
   });
-
-  test("CacheMoney should have traverse function", () => {
-    const cacheMoney = new CacheMoney(endpoint, capacity, groupSize, redisClient);
-    expect(typeof cacheMoney.traverse).toBe('function');
-  });
-
-  describe('checkCache', () => {
-    beforeEach(() => {
-        const req = { 
-            body: {
-                query: 'some query',
-                variables: {some: 'variables'},
-            }
-        };
-        res = jest.fn();
-        next = jest.fn();
-    });
-
-    test('should set valueFromCache if redisClient is used', async () => {
-        const redisClient = {
-            get: jest.fn().mockReturnValue('someValue')
-        }
-        await checkCache(req, res, next);
-        expect(redisClient.get).toHaveBeenCalled();
-        expect(valueFromCache).toEqual('someValue');
-    });
-
-    test('should set valueFromCache if cacheMoneyCache is used', async () => {
-        await checkCache(req, res, next);
-        expect(cacheMoneyCache[cacheKey]).toBeDefined();
-        expect(valueFromCache).toEqual(cacheMoneyCache[cacheKey]);
-    });
 });
 
 
@@ -105,8 +105,8 @@ describe('Testing if cache contains the requested data', () => {
     }
   });
 });
- //accounts for if the minLatencyNodeInGroup is the tail of the list
- if (this.tail === minLatencyNodeInGroup) {
+//accounts for if the minLatencyNodeInGroup is the tail of the list
+if (this.tail === minLatencyNodeInGroup) {
   this.tail = this.tail.prev;
   this.tail.next = null;
   this.length--;
@@ -124,35 +124,35 @@ return minLatencyNodeInGroup;
 
 
 describe("add()", () => {
-let evictionQueue;
-beforeEach(() => {
-evictionQueue = new EvictionQueue();
-});
+  let evictionQueue;
+  beforeEach(() => {
+    evictionQueue = new EvictionQueue();
+  });
 
-it("Should add a new node to the head of the list", () => {
-const cacheKey = "test";
-const latency = 10;
-evictionQueue.add(cacheKey, latency);
-expect(evictionQueue.head.latency).toBe(10);
-expect(evictionQueue.head.key).toBe("test");
-});
+  it("Should add a new node to the head of the list", () => {
+    const cacheKey = "test";
+    const latency = 10;
+    evictionQueue.add(cacheKey, latency);
+    expect(evictionQueue.head.latency).toBe(10);
+    expect(evictionQueue.head.key).toBe("test");
+  });
 });
 
 describe("removeSmallestLatencyFromGroup()", () => {
-let evictionQueue;
-beforeEach(() => {
-evictionQueue = new EvictionQueue();
-});
+  let evictionQueue;
+  beforeEach(() => {
+    evictionQueue = new EvictionQueue();
+  });
 
-it("Should return the node with the smallest latency from the group", () => {
-evictionQueue.add("test1", 5);
-evictionQueue.add("test2", 10);
-evictionQueue.add("test3", 8);
-const minLatencyNodeInGroup = evictionQueue.removeSmallestLatencyFromGroup(3);
-expect(minLatencyNodeInGroup.key).toBe("test1");
-expect(minLatencyNodeInGroup.latency).toBe(5);
-expect(evictionQueue.length).toBe(2);
-});
+  it("Should return the node with the smallest latency from the group", () => {
+    evictionQueue.add("test1", 5);
+    evictionQueue.add("test2", 10);
+    evictionQueue.add("test3", 8);
+    const minLatencyNodeInGroup = evictionQueue.removeSmallestLatencyFromGroup(3);
+    expect(minLatencyNodeInGroup.key).toBe("test1");
+    expect(minLatencyNodeInGroup.latency).toBe(5);
+    expect(evictionQueue.length).toBe(2);
+  });
 });
 describe("updateRecencyOfExistingCache", () => {
   it("should update the recency of an existing cache", () => {
